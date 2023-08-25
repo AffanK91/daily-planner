a1bf846c1215c917146e1ca3687664669114128b
Affan Kamal <affan.kamal91@gmail.com>
2023-08-24 21:47:16 -0400

    updated JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,28 @@ function displayTime() {
   timeDisplayEl.text(rightNow);
 }
 
-function getTextDesc() {
-  textAreaEl = localStorage.getItem('text');
-  if (textAreaEl) {
-    textAreaEl = JSON.parse(text);
+$('.saveBtn').on('click', function(){
+  var value = $(this).siblings('.description').val();
+  var time = $(this).parent().attr('id');
 
-    return textAreaEl
-
-}
-}
-function saveTextDesc(textAreaEl) {
-  localStorage.setItem('text', JSON.stringify(textAreaEl));
-}
+localStorage.setItem(time, value);
+})
 
 displayTime();
 setInterval(displayTime, 1000);
 
-saveBtnEl.on('click', getTextDesc)
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-1 .description').val(localStorage.getItem('hour-1'));
+$('#hour-2 .description').val(localStorage.getItem('hour-2'));
+$('#hour-3 .description').val(localStorage.getItem('hour-3'));
+$('#hour-4 .description').val(localStorage.getItem('hour-4'));
+$('#hour-5 .description').val(localStorage.getItem('hour-5'));
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -49,6 +43,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
 
-});
+
 
 
